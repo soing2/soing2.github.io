9a1508c3986f6678dccd637a8d4d3ff2d67920d7
Junki Kim <wishoping@gmail.com>
2015-03-05 22:21:55 +0900

    update popup click action

--- js/control.js
+++ js/control.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
 });
 
 
-function showPopup() {
+function showPopup(pfName) {
+    $(".popup_container .popup .pf").hide();
     $(".popup_container").show();
+    $(".popup_container .popup ." + pfName).show();
 }
 
 function hidePopup() {
